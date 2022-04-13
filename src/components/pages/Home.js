5870cd812ed2d3b9704dd0af4fa13787e65a6139
Goga <gogaimerlishvili1@gmail.com>
2022-04-13 16:51:53 +0400

    Added Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
+import PageLayout from "../PageLayout";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <PageLayout>Home</PageLayout>;
 };
 
 export default Home;
