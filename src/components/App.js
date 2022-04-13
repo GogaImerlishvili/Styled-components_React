684e70d632a4cb6de03163df74357f5dab027f88
Goga <gogaimerlishvili1@gmail.com>
2022-04-13 15:38:52 +0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,26 @@
+import { Button } from "../components/common";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background:white;
+    min-height: 100vh;
+    margin:0;
+    color:black;
+    font-family: 'Kaushan Script';
+  }
+`;
+
 function App() {
-  return <div className="App">f</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>App</h1>
+      <Button priamry>Primary button</Button>
+      <Button secondary>Secondary button</Button>
+      <Button large>Disabled button</Button>
+    </>
+  );
 }
 
 export default App;
