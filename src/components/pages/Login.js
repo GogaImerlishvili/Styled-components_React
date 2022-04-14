a9ed3aafa37c98800b22f7ca8fe18079dab42563
Goga <gogaimerlishvili1@gmail.com>
2022-04-14 10:57:37 +0400

    Setup form input

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,25 @@
 import React from "react";
 import PageLayout from "../PageLayout";
+import styled from "styled-components";
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 1px solid #eee;
+  padding: 16px;
+  box-sizing: border-box;
+  color: black;
+  border-radius: 4px;
+`;
 
 const Login = () => {
-  return <PageLayout>Login</PageLayout>;
+  return (
+    <PageLayout>
+      <h1>Login</h1>
+      <Form></Form>
+    </PageLayout>
+  );
 };
 
 export default Login;
