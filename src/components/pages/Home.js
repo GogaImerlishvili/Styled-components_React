69129320f4362d8d8f712244dfe0332b355d6a48
Goga <gogaimerlishvili1@gmail.com>
2022-04-14 21:51:23 +0400

    Initial commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,46 @@
-import React, { useState } from "react";
-import PageLayout from "../PageLayout";
+import React from "react";
+import { PageLayout } from "../common/PageLayout";
 
-const Home = () => {
+export default function Home() {
   return (
     <PageLayout>
       <h1>Home</h1>
+      <p>
+        3 wolf moon put a bird on it sartorial vinyl normcore palo santo forage
+        vape ramps pug hashtag master cleanse letterpress tofu. Schlitz fam
+        helvetica, you probably haven't heard of them post-ironic kickstarter
+        yuccie. Irony vape blog, XOXO bushwick raclette mixtape succulents
+        gentrify godard kinfolk keytar banh mi church-key neutra. Taxidermy swag
+        fixie raclette irony. Leggings austin forage, yuccie direct trade
+        fashion axe fam seitan prism whatever freegan pop-up pabst post-ironic
+        hell of. Crucifix artisan celiac mixtape, mustache letterpress tilde
+        gentrify distillery live-edge woke meh shaman everyday carry.
+      </p>
+      <p>
+        Unicorn tumeric hell of plaid, raw denim lomo semiotics gochujang
+        succulents tote bag selfies man bun. Pop-up heirloom meh occupy butcher
+        direct trade blue bottle tote bag schlitz. Offal try-hard mumblecore af
+        shoreditch. Godard artisan retro, biodiesel umami cloud bread unicorn.
+        Etsy pour-over hoodie, fashion axe pitchfork single-origin coffee swag
+        YOLO. Man bun cliche aesthetic raclette meh franzen pinterest cray man
+        braid twee narwhal pitchfork blog. Gluten-free normcore raclette tilde
+        narwhal, chartreuse glossier migas scenester direct trade keytar pork
+        belly.
+      </p>
+      <p>
+        Microdosing food truck hoodie jean shorts wolf dreamcatcher tofu
+        church-key blue bottle distillery. Af bespoke narwhal pickled austin
+        taiyaki, tofu fam tousled stumptown cronut activated charcoal. Roof
+        party adaptogen you probably haven't heard of them art party,
+        dreamcatcher mixtape ugh keffiyeh etsy lyft affogato snackwave chia
+        cold-pressed. Humblebrag seitan kombucha succulents fingerstache bitters
+        health goth direct trade drinking vinegar intelligentsia vinyl tattooed
+        slow-carb tacos. Vaporware jianbing tote bag polaroid man braid copper
+        mug microdosing direct trade put a bird on it mixtape godard vinyl
+        cliche try-hard. Art party DIY artisan lumbersexual, kickstarter hell of
+        viral. Vexillologist blue bottle paleo freegan plaid +1 chicharrones
+        narwhal.
+      </p>
     </PageLayout>
   );
-};
-
-export default Home;
+}
