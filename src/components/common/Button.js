072a1244cba9362babe58379a1acdc29887945bc
Goga <gogaimerlishvili1@gmail.com>
2022-04-14 21:47:48 +0400

    Proptypes and interpolation

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,31 +1,43 @@
 import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
 
-export const Button = styled.button`
+const largeStyles = ({ large }) => {
+  if (large) {
+    return css`
+      padding: 10px;
+      border-radius: 5px;
+      font-size: 1.5em;
+    `;
+  } else {
+    return css`
+      padding: 8px;
+      border-radius: 4px;
+      font-size: 1em;
+    `;
+  }
+};
+
+const Button = styled.button`
   color: white;
-  cursor: pointer;
   background: ${(p) =>
     p.secondary ? p.theme.secondaryColor : p.theme.primaryColor};
   font-weight: bold;
-  ${(p) =>
-    p.large
-      ? css`
-          padding: 10px;
-          border-radius: 5px;
-          font-size: 1.5em;
-        `
-      : css`
-          padding: 8px;
-          border-radius: 4px;
-          font-size: 1em;
-        `}
+  ${largeStyles}
 
+  box-shadow: none;
   border: none;
   width: 100%;
   display: block;
   white-space: none;
-
   &:disabled {
     background: #eee;
     color: #666;
   }
 `;
+
+Button.propTypes = {
+  large: PropTypes.bool,
+  secondary: PropTypes.bool,
+};
+
+export { Button };
