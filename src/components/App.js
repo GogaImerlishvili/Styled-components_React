11fb17060aa49bc35888277228b46bac46e66185
Goga <gogaimerlishvili1@gmail.com>
2022-04-14 21:46:53 +0400

    Body and nav styles for themes

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import LightTheme from "../themes/light";
 import DarkTheme from "../themes/dark";
+
 const GlobalStyle = createGlobalStyle`
-  body{
-    background:white;
-    min-height: 100vh;
-    margin:0;
-    color:black;
-    font-family: 'Kaushan Script';
-  }
+	body{
+		background: ${(p) => p.theme.bodyBackgroundColor};
+		min-height: 100vh;
+		margin: 0;
+		color: ${(p) => p.theme.bodyFontColor};
+		font-family: 'Kaushan Script'
+	}
 `;
 
 function App() {
   const [theme, setTheme] = useState(LightTheme);
+
   return (
     <ThemeProvider
       theme={{
@@ -27,12 +29,12 @@ function App() {
       }}
     >
       <GlobalStyle />
-      <Router>
+      <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/" exact element={<Home />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
